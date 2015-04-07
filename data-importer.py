cd9f7a6e71f2601f1bc819452823e4aba96776ad
Marsfan <Marsfan@users.noreply.github.com>
2015-04-07 14:18:59 -0500

    added comments

--- data-importer.py
+++ data-importer.py
@@ -7,7 +7,7 @@ city = "Dallas"
 settings = "lang:EN/pws:1/bestfct:1"
 
 #standard format for url is api.wunderground.com/api/"API KEY"/"FEATURES"/"SETTINGS"/q/"QUERY.format.
-#API KEY is teh api key that you got on the wunderground website.
+#API KEY is the api key that you got on the wunderground website.
 #FEATURES is the data features that one can request. See documentation on the wunderground site for a list of all features
 #Settings is optional, defines some settings: Language, use of Personal Weather stations, Use of wunderground best forcast.
 #See the Documentation for more features
@@ -17,11 +17,16 @@ settings = "lang:EN/pws:1/bestfct:1"
 
 def getConditions():
     while True:
+    	#set the url for the wunderground api data. The apikey variable in the middle references the key that was entered earlier.
         request = urllib2.Request("http://api.wunderground.com/api/" + apikey + "/conditions/q/autoip.json")
-        conditions = urllib2.urlopen(request)
-        parseddata = json.load(conditions)
+        #download the data requested by teh previous line
+        response = urllib2.urlopen(request)
+        #read the json file from the request and save the data to conditions object
+        conditions = json.load(response)
         #TODO: Add extraction and labeling process
-	print time.localtime()
+        #TEST: extract the data keys found in the conditions object. 
+	print conditions.keys()
+	#wait for three minutes, change this if you have a better api plan and request more data. 
         time.sleep(180)
-
+#Run the get conditions function: This will be removed once I in
 getConditions()
