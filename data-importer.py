e98763db2cf9dd39798e5b192734ed4a723ae191
Marsfan <marsfan@users.noreply.github.com>
2015-04-10 15:07:24 -0500

    figured out how to get extract info, beginning function to do so

--- data-importer.py
+++ data-importer.py
@@ -6,22 +6,30 @@ import requests, time
 
 
 def getConditions():
+    #make some variables global to allow accessing from other functionschrom
     global apikey
+    #set the name of the file with the api key
     apifile = open("api.cfg")
+    #save api key to a variable
     apikey = apifile.read()
+    #close the file to allow it to be accessed by other progams
     apifile.close()
     #loop endlessly so that we can keep getting data
     #TODO: replace this with something that makes it eaiser for
     #other scripts to terminate the process
     while True:
-    	#set the url for the wunderground api data. The apikey variable in the middle references the key that was entered earlier.
+    	#download weather conditions. 
         response = requests.get("http://api.wunderground.com/api/" + apikey + "/conditions/q/autoip.json")
         #read the json file from the request and save the data to conditions object
         conditions = response.json()
-        #TODO: Add extraction and labeling process
-        #TEST: print out the keys for the current observation data.
-	print conditions.keys()
+        #TEST: print out the current tempature in f
+	print conditions['current_observation']['temp_f']
 	#wait for three minutes, change this if you have a better api plan and request more data. 
         time.sleep(180)
 #Run the get conditions function: This will be removed once I in
 getConditions()
+
+
+#TODO: Finish extraction process
+def getData(section, key):
+	return conditions['curr
